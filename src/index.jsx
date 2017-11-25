b078f25854f146e4b855bc8c1ce6c9bb55a97d98
nishina <n.toshi.1.014@gmail.com>
2017-11-25 18:25:34 +0900

    hello world

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Hello } from './Hello';
+import { Provider } from 'react-redux';
+import App from './component/App';
 
 ReactDOM.render(
-  <Hello />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
